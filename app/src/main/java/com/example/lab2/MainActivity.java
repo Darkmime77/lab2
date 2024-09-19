f6ace88e37329af3cbabbbe43cda45a785f83a8a
Darkmime77 <ilya_hleb@mail.ru>
2024-09-19 19:46:27 +0500

    add

---
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.lab2;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonP, buttonM, buttonY, buttonD, buttonC;
+    EditText editText1, editText2;
+    TextView textView1, textView2;
+    ConstraintLayout constraintLayout;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        buttonP = findViewById(R.id.button);
+        buttonM = findViewById(R.id.button2);
+        buttonY = findViewById(R.id.button5);
+        buttonD = findViewById(R.id.button3);
+        buttonC = findViewById(R.id.button4);
+
+        editText1 = findViewById(R.id.editTextNumber);
+        editText2 = findViewById(R.id.editTextNumber1);
+
+        textView1 = findViewById(R.id.textView3);
+        textView2 = findViewById(R.id.textView5);
+
+        constraintLayout = findViewById(R.id.l1);
+
+        editText1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                constraintLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        editText2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                constraintLayout.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
